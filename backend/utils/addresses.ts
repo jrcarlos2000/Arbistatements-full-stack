c712e403c4e3e94773683fae528e862a61d4e0e9
CarlosR <carlos.ramos@grupoimportia.com>
2022-10-15 21:12:56 -0500

    fixed addresses

--- backend/utils/addresses.ts
+++ backend/utils/addresses.ts
@@ -1,6 +1,6 @@
 export const addresses : any= {
     ChainStatements : {
-        421613 : "0x599AF7706a7C881b0B9C9585cCc1486EDc990887",
+        421613 : "0x45a2DFF9569F7fAA29015897ea956A5A48068273",
         1337 : "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9"
     }
 }
